0f6badf26263e2c7a868faeb42e6e068eaf9d67f
NikhilEvra <nikhil@evramedia.com>
2023-06-21 09:16:45 +0530

    api backup

---
+++ admin/notification.php
@@ -0,0 +1,28 @@
+<?php
+ header('Access-Control-Allow-Origin: *');  
+ include ('config.php');
+
+$response = array();
+if($con){
+    $sql = "select * from notifications where `status` = 'Active' ORDER BY id DESC ";
+    $result = mysqli_query($con,$sql);
+    if($result){
+        $x = 0;
+
+        while($row = mysqli_fetch_assoc($result)) {
+            $response [$x]['id'] = $row['id'];
+            $response [$x]['message'] = $row['message'];
+            $response [$x]['panel'] = $row['panel'];
+            $response [$x]['status'] = $row['status'];
+            $response [$x]['date'] = $row['date'];
+            $response [$x]['time'] = $row['time'];
+            $x++;
+            
+        }
+        echo json_encode($response,JSON_PRETTY_PRINT);
+    }
+}else{
+    echo "error connecting database";
+}
+
+?>
