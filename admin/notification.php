7573b0598f0ab9392a4e4b52622debc3315c9146
NikhilEvra <nikhil@evramedia.com>
2023-06-22 17:58:29 +0530

    notify api updated

--- admin/notification.php
+++ admin/notification.php
@@ -13,6 +13,7 @@ if($con){
             $response [$x]['id'] = $row['id'];
             $response [$x]['message'] = $row['message'];
             $response [$x]['panel'] = $row['panel'];
+            $response [$x]['sender_panel'] = $row['sender_panel'];
             $response [$x]['status'] = $row['status'];
             $response [$x]['date'] = $row['date'];
             $response [$x]['time'] = $row['time'];
