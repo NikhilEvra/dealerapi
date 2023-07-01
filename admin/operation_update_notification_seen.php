1ed8d6e8f9053584e7373524f51c6ef241cec098
NikhilEvra <nikhil@evramedia.com>
2023-07-01 17:47:29 +0530

    apis for services panel

--- admin/operation_update_notification_seen.php
+++ admin/operation_update_notification_seen.php
@@ -12,10 +12,11 @@ $d = date("Y/m/d");
 $t = date("h:i:sa");
 
 $id = $_POST['id'];
+$po_id = $_POST['po_id'];
 
 if($con){ 
-    $sql = "UPDATE `notifications` SET `seen`='Seen' WHERE id = '$id'";
-    $sql2 = "INSERT INTO `notifications_entry`(`id`, `n_id`, `date`, `time`) VALUES (Null,'$id','$d','$t')";
+    $sql = "UPDATE `notifications` SET `seen` = 'Seen' WHERE id = '$id' " ; 
+    $sql2 = "INSERT INTO `notifications_entry`(`id`, `n_id`, `date`, `time`, `po_id`) VALUES (Null,'$id','$d','$t','$po_id')";
     $result = mysqli_query($con,$sql);
     $result2 = mysqli_query($con,$sql2);
     if($result && $result2){
@@ -26,4 +27,4 @@ if($con){
 }
 
 
-?>
+?> 
